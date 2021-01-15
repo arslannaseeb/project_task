b199782d575b1c32b8c45541fa7b9511e26b9e7c
arslannaseebdev <arslan.naseeb2@gmail.com>
2021-01-15 16:16:45 +0500

    commited

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Maatwebsite\Excel\Facades\Excel;
+use App\Models\Project;
+use Illuminate\Http\Request;
+use App\Imports\ProjectsImport;
+use Illuminate\Support\Facades\Auth;
+
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $projects = Project::latest()->paginate(5);
+
+        return view('projects.index', compact('projects'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'introduction' => 'required',
+            'location' => 'required',
+            'cost' => 'required'
+        ]);
+
+        $projectName = $request->name;
+
+        Project::create($request->all());
+
+        return redirect()->route('projects.index')
+            ->with('success', $projectName. ' created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Project $project)
+    {
+        return view('projects.show', compact('project'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Project  $project
+     * @return \Illuminate\Http\Response
+     */
+    
+    public function importProject(){
+        Excel::import(new ProjectsImport, request()->file('file'));
+
+        return back()->with('success','project created ');
+
+    }
+}
